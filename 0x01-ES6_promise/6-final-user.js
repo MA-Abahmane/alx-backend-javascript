992fdc3a4f299531e792e86cd704f135052a528a
Mohamed Amine <ma.abahmane@gmail.com>
2023-11-28 23:05:50 +0100

    update task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,6 @@
-import singUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import singUpUser from './4-user-promise'; import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([singUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((message) => message.map((result) => ({
