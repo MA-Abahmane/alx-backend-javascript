b05209ecbc0bae212135b02a208e31b8309f0f59
Mohamed Amine <ma.abahmane@gmail.com>
2023-11-28 23:00:23 +0100

    pudate task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,10 +3,8 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
-    .then((message) => {console.log(`
-${message[0].body} \
-${message[1].firstName} \
-${message[1].lastName}`);
+    .then((message) => {
+      console.log(`${message[0].body} ${message[1].firstName} ${message[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
