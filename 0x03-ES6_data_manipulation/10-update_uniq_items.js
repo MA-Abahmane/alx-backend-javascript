da12549bde44bd0f55e5d99c82c8ff6343de2081
Mohamed Amine <ma.abahmane@gmail.com>
2023-12-04 17:51:09 +0100

    update Task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,7 @@
 export default function updateUniqueItems(map) {
-  if (map instanceof Map) {
-    for (const [key, val] of map.entries()) {
-      if (val === 1) map.set(key, 100);
-    }
+  if (!(map instanceof Map)) throw new Error('Cannot process');
+
+  for (const [key, val] of map.entries()) {
+    if (val === 1) map.set(key, 100);
   }
 }
