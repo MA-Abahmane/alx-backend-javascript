f1035bdbeb7746ed34ef94d4f621043bf38a54e2
Mohamed Amine <ma.abahmane@gmail.com>
2024-01-29 22:21:35 +0100

    Update Task 6

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,21 +1,22 @@
 /**
- * Install Express and in a file named 6-http_express.js, create a small HTTP server using Express module:
+ * Install Express and in a file named 6-http_express.js, create a small HTTP server
+ *   using Express module:
  *
  *  It should be assigned to the variable app and this one must be exported
  *  HTTP server should listen on port 1245
  *  Displays Hello Holberton School! in the page body for the endpoint /
  */
 
-const exp = require('express');
+const exp = require('express')
 
-const app = exp();
-const port = 1245;
+const app = exp()
+const port = 1245
 
 // Start application server
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
+  res.send('Hello Holberton School!')
+})
 
-app.listen(port);
+app.listen(port)
 
-module.exports = app;
+module.exports = app
