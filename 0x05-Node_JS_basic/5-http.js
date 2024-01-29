6f5b48e9506293fa70c4e83e839c48e75731cedf
Mohamed Amine <ma.abahmane@gmail.com>
2024-01-29 21:56:09 +0100

    Update Task 5 II

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,41 +1,30 @@
-/**
- * In a file named 5-http.js, create a small HTTP server using the http module:
- *
- *  It should be assigned to the variable app and this one must be exported
- *  HTTP server should listen on port 1245
- *  It should return plain text
- *  When the URL path is /, it should display Hello Holberton School! in the page body
- *  When the URL path is /students, it should display This is the list of our students
- *  followed by the same content as the file
- *    3-read_file_async.js (with and without the database) - the name of the database
- *       must be passed as argument of the file
- *  CSV file can contain empty lines (at the end) - and they are not a valid student!
- */
+const express = require('express');
+const app = express();
 
-const exp = require('express');
 const countStudents = require('./3-read_file_async');
 
-const app = exp();
-const port = 1245;
-const file = process.argv.slice(2)[0];
+const args = process.argv.slice(2);
+const DATABASE = args[0];
 
-// Start application server
 app.get('/', (req, res) => {
-  res.type('text/plain');
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-  res.type('text/plain');
-  countStudents(file)
-    .then((data) => {
-      res.end(`This is the list of our students\n${data}`);
-    })
-    .catch((error) => {
-      res.end(error.message);
-    });
+app.get('/students', async (req, res) => {
+  try {
+    const students = await countStudents(DATABASE);
+    res.send(`This is the list of our students\n${students.join('\n')}`);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
-app.listen(port);
+app.use((req, res) => {
+  res.status(404).send('Not found');
+});
 
-module.exports = app;
+const server = app.listen(1245, () => {
+  // console.log('Server listening on port 1245');
+});
+
+module.exports = server;
