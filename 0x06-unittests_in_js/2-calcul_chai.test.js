8974d585cdb1d3f8c878a46c8eef7fa88198f7fe
Mohamed Amine <ma.abahmane@gmail.com>
2024-02-01 14:17:56 +0100

    test Task 2

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,35 +1,25 @@
-const expect = require('chai').expect;
+'use strict';
+const chai = require('chai');
 
-const calculateNumber = require("./2-calcul_chai.js");
+const calculateNumber = require('./2-calcul_chai.js');
 
 
-describe('Test calculateNumber V2 with chai', () => {
-  it("Test SUM operation", () => {
-    expect(calculateNumber('SUM', 1, 3)).to.equal(4);
-    expect(calculateNumber('SUM', 5.5, 2.5)).to.equal(8);
-    expect(calculateNumber('SUM', -3, 7)).to.equal(4);
-  });
-
-  it("Test SUBTRACT operation", () => {
-    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-3);
-    expect(calculateNumber('SUBTRACT', 10, 5)).to.equal(5);
-    expect(calculateNumber('SUBTRACT', -5, -3)).to.equal(-2);
-  });
-
-  it("Test DIVIDE operation", () => {
-    expect(calculateNumber('DIVIDE', 5, 2)).to.equal(2.5);
-    expect(calculateNumber('DIVIDE', 9, 3)).to.equal(3);
-    expect(calculateNumber('DIVIDE', -6, 2)).to.equal(-3);
+describe('calculateNumber test', () => {
+  it('checks the output', () => {
+    chai.expect(calculateNumber('SUBTRACT', 2, 4.5)).to.equal(-3);
+    chai.expect(calculateNumber('SUBTRACT', 4.5, 2)).to.equal(3);
+    chai.expect(calculateNumber('SUBTRACT', -1.5, 0)).to.equal(-1);
+    chai.expect(calculateNumber('SUBTRACT', -1, 1)).to.equal(-2);
+    chai.expect(calculateNumber('SUBTRACT', 3.1, 2.5)).to.equal(0);
   });
 });
 
-
-describe('Test Error cases', () => {
-  it("Test Error case - Division by zero", () => {
-    expect(() => calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
-  });
-
-  it("Test Error case - Invalid operation type", () => {
-    expect(() => calculateNumber('DIVIDES', 1, 1)).to.equal(0);
+describe('calculateNumber test', () => {
+  it('checks the output', () => {
+    chai.expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    chai.expect(calculateNumber('SUM', -1, 1)).to.equal(0);
+    chai.expect(calculateNumber('SUM', 1, -1)).to.equal(0);
+    chai.expect(calculateNumber('SUM', 0.0, 0)).to.equal(0);
+    chai.expect(calculateNumber('SUM', 3.7, 1)).to.equal(5);
   });
 });
