fa7026b09e2300ff4d2ca5568c3020525b557ec3
Mohamed Amine <ma.abahmane@gmail.com>
2024-02-01 17:37:37 +0100

    update Tasks 10

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -8,6 +8,20 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
+app.get('/available_payments', (req, res) => {
+    res.send({
+        payment_methods: {
+          credit_cards: true,
+          paypal: false
+        }
+      });
+});
+
+app.post('/login', (req, res) => {
+    res.send(`Welcome Betty`);
+});
+
+
 app.listen(port, () => {
     console.log('API available on localhost port 7865');
 });
